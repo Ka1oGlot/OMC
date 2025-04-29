a502ef1ba766c81c92f12500c76b5cc273b3c23d
Ka1oGlot <tsnazaros@gmail.com>
2025-04-29 14:53:40 +0300

    modified-generator

--- RandomNumbersGenerator/random_numbers_generator.cpp
+++ RandomNumbersGenerator/random_numbers_generator.cpp
@@ -3,23 +3,23 @@
 #include <cstdlib>
 
 int rand(int low, int high) {
-    return low + std::rand() % (high - low + 1);
+    return std::rand() % ((high + 1) - low) + low;
 }
 
 int main(){
     int low, high;
+    srand(static_cast<unsigned>(time(nullptr)));
 
-    std::cout << "Введіть нижню межу (low): ";
+    std::cout << "Enter low edge: ";
     std::cin >> low;
-    std::cout << "Введіть верхню межу (high): ";
+    std::cout << "Enter high edge: ";
     std::cin >> high;
 
     if (low > high) {
-        std::cerr << "Помилка: нижня межа більша за верхню!\n";
+        std::cerr << "Error: lower edge is bigger than higher!\n";
         exit(EXIT_FAILURE);
     }
-
-    srand(static_cast<unsigned>(time(nullptr) / 0.24));
-    std::cout << std::rand() << std::endl;
+    
+    std::cout << rand(low, high) << std::endl;
     return 0;
 }
